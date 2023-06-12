bc071e474b9347a16a97b87f97b701a753eca868
Jerricke <eejerrickw@gmail.com>
2023-06-11 21:57:50 -0600

    complete

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    return "This one is on me!"
+  } else if (ride > 400 && ride <= 2000) {
+    return "That will be twenty bucks."
+  } else if (ride > 2000 && ride <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityname){
   // Write your code here!
+
+  const itworks = cityname === "NYC" ? "Ok, sounds good." : "No go."
+  return itworks
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  switch(tip){
+    case "generous" :
+      message = "Thank you so much.";
+      break;
+    case "not as generous" :
+      message = "Thank you.";
+      break;
+    default: 
+    message = "Bye."
+    break;    
+  } return message;
 }
